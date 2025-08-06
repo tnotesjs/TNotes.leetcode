74aa90812174286f826cc3496327c8ee6e41c300
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-08-06 23:37:13 +0800

    update: 14 files modified

---
+++ .leetcode/1.两数之和.c
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=c
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for (int i = 0; i < numsSize - 1; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (target == nums[i] + nums[j]) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
+// @lc code=end
+
