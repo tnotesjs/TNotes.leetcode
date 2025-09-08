a3c6019a0921bb2e0857798bf22dba112e26f854
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-09-08 21:00:14 +0800

    update: 9 files modified

--- .leetcode/674.最长连续递增序列.js
+++ .leetcode/674.最长连续递增序列.js
@@ -33,34 +33,38 @@ var findLengthOfLCIS = function (nums) {
 }
 // @lc code=end
 
-// 暴力解法：
-// 上坡不断增加 currentLen，并跟当前的 maxLen 进行比较，如果当前长度大于 maxLen，则更新 maxLen
-// 一旦遇到下坡，则重置 currentLen
-// 时间复杂度：$O(n)$，其中 n 是数组长度，需要遍历数组一次。
-// 空间复杂度：$O(1)$，只使用了常数级别的额外变量空间。
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var findLengthOfLCIS = function (nums) {
-//   if (nums.length === 0) {
-//     return 0
-//   }
+// #region 1
+/* 
+暴力解法：
+上坡不断增加 currentLen，并跟当前的 maxLen 进行比较，如果当前长度大于 maxLen，则更新 maxLen
+一旦遇到下坡，则重置 currentLen
+时间复杂度：$O(n)$，其中 n 是数组长度，需要遍历数组一次。
+空间复杂度：$O(1)$，只使用了常数级别的额外变量空间。
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+  if (nums.length === 0) {
+    return 0
+  }
 
-//   let maxLen = 1 // 至少有一个元素
-//   let currentLen = 1 // 当前连续递增序列长度
+  let maxLen = 1 // 至少有一个元素
+  let currentLen = 1 // 当前连续递增序列长度
 
-//   for (let i = 1; i < nums.length; i++) {
-//     if (nums[i] > nums[i - 1]) {
-//       // 如果当前元素大于前一个元素，递增序列继续
-//       currentLen++
-//       // 更新最大长度
-//       maxLen = Math.max(maxLen, currentLen)
-//     } else {
-//       // 如果不满足递增条件，重置当前长度
-//       currentLen = 1
-//     }
-//   }
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      // 如果当前元素大于前一个元素，递增序列继续
+      currentLen++
+      // 更新最大长度
+      maxLen = Math.max(maxLen, currentLen)
+    } else {
+      // 如果不满足递增条件，重置当前长度
+      currentLen = 1
+    }
+  }
 
-//   return maxLen
-// }
+  return maxLen
+}
+// #endregion
--- notes/0674. 最长连续递增序列【简单】/solutions/1/1.js
+++ notes/0674. 最长连续递增序列【简单】/solutions/1/1.js
@@ -1,1 +1,26 @@
-// todo
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+  if (nums.length === 0) {
+    return 0
+  }
+
+  let maxLen = 1 // 至少有一个元素
+  let currentLen = 1 // 当前连续递增序列长度
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      // 如果当前元素大于前一个元素，递增序列继续
+      currentLen++
+      // 更新最大长度
+      maxLen = Math.max(maxLen, currentLen)
+    } else {
+      // 如果不满足递增条件，重置当前长度
+      currentLen = 1
+    }
+  }
+
+  return maxLen
+}
