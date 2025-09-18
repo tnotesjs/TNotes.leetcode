7439da5e5ee6d64f1a290a75e0643d4a26267bd6
Tdahuyou <dahuyou_top@163.com>
2025-09-18 11:06:24 +0800

    update

--- .vitepress/tnotes/utils/get_changed_ids.js
+++ .vitepress/tnotes/utils/get_changed_ids.js
@@ -1,16 +1,16 @@
+import path from 'path'
 import { execSync } from 'child_process'
 
 export function getChangedIds() {
   const changedFiles = execSync(
-    `git diff --name-only HEAD~1 HEAD -- 'notes/[0-9][0-9][0-9][0-9]*/README.md'`
+    `git diff --name-only HEAD~1 HEAD -- "notes/[0-9][0-9][0-9][0-9]*/README.md"` // <-- 这里从单引号改为了双引号
   )
     .toString()
     .split(/\r?\n/)
     .filter(Boolean)
+    .map((fp) => fp.replace(/^"|"$/g, '')) // 去掉 Git 输出的双引号
+    .map((fp) => fp.split('/').join(path.sep)) // 转换为平台路径
 
-  /**
-   * 本次更新的笔记 ID 集合
-   */
   const changedIds = changedFiles
     .map((fp) => {
       const parts = fp.split(path.sep)
@@ -20,6 +20,5 @@ export function getChangedIds() {
     .filter(Boolean)
 
   console.log('本次更新的笔记 ID 集合:', changedIds)
-
   return new Set(changedIds)
 }
