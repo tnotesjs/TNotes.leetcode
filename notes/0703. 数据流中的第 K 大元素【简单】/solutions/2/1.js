9eeddb761ba5e2429fccf8cf8c3c1d99b980143a
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-09-23 08:01:56 +0800

    update: 8 files modified

--- notes/0703. 数据流中的第 K 大元素【简单】/solutions/2/1.js
+++ notes/0703. 数据流中的第 K 大元素【简单】/solutions/2/1.js
@@ -1,1 +1,31 @@
-// todo
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function (k, nums) {
+  this.k = k
+  // 使用最小堆
+  this.minHeap = new MinPriorityQueue()
+
+  // 初始化堆
+  for (const num of nums) {
+    this.add(num)
+  }
+}
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {
+  // 向堆中添加元素
+  this.minHeap.enqueue(val)
+
+  // 如果堆的大小超过k，移除最小元素
+  if (this.minHeap.size() > this.k) {
+    this.minHeap.dequeue()
+  }
+
+  // 返回第k大的元素（堆顶）
+  return this.minHeap.front()
+}
