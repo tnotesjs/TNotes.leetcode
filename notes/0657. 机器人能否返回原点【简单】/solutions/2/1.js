363f8a91caf0b9e83fc35edf38d3b865bf619c43
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-09-25 22:51:14 +0800

    update: 72 files modified

--- notes/0657. 机器人能否返回原点【简单】/solutions/2/1.js
+++ notes/0657. 机器人能否返回原点【简单】/solutions/2/1.js
@@ -1,1 +1,12 @@
-// todo
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function (moves) {
+  const countCharRegex = (char) =>
+    (moves.match(new RegExp(char, 'g')) || []).length
+  return (
+    countCharRegex('U') === countCharRegex('D') &&
+    countCharRegex('R') === countCharRegex('L')
+  )
+}
--- notes/0844. 比较含退格的字符串【简单】/solutions/2/1.js
+++ notes/0844. 比较含退格的字符串【简单】/solutions/2/1.js
@@ -1,1 +1,54 @@
-// todo
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function (s, t) {
+  let i = s.length - 1 // s的指针 - 确保指向有效字符
+  let j = t.length - 1 // t的指针 - 确保指向有效字符
+  let skipS = 0 // s中需要跳过的字符数
+  let skipT = 0 // t中需要跳过的字符数
+
+  while (i >= 0 || j >= 0) {
+    // 在s中找到下一个有效字符
+    while (i >= 0) {
+      if (s[i] === '#') {
+        skipS++
+        i--
+      } else if (skipS > 0) {
+        skipS--
+        i--
+      } else {
+        break
+      }
+    }
+
+    // 在t中找到下一个有效字符
+    while (j >= 0) {
+      if (t[j] === '#') {
+        skipT++
+        j--
+      } else if (skipT > 0) {
+        skipT--
+        j--
+      } else {
+        break
+      }
+    }
+
+    // 比较两个有效字符
+    if (i >= 0 && j >= 0 && s[i] !== t[j]) {
+      return false
+    }
+
+    // 如果一个字符串已经遍历完，另一个还有有效字符，则不相等
+    if (i >= 0 !== j >= 0) {
+      return false
+    }
+
+    i--
+    j--
+  }
+
+  return true
+}
