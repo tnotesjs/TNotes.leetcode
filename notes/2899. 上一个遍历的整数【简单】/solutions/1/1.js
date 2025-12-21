2258535fdc0c2f40ff20e6aaa3a94dea3107fe0f
Tdahuyou <dahuyoutop@gmail.com>
2025-12-21 21:23:27 +0800

    📝 Update notes - 2025-12-21 21:23:26

--- notes/2899. 上一个遍历的整数【简单】/solutions/1/1.js
+++ notes/2899. 上一个遍历的整数【简单】/solutions/1/1.js
@@ -1,1 +1,27 @@
-// todo
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var lastVisitedIntegers = function (nums) {
+  const seen = [] // 存储正整数
+  const ans = []
+  let k = 0 // 连续 -1 的计数
+
+  for (const num of nums) {
+    if (num === -1) {
+      k++
+      // k 超过 seen 长度，返回 -1
+      if (k > seen.length) {
+        ans.push(-1)
+      } else {
+        // seen 的第 k 个元素（从头部开始）
+        ans.push(seen[k - 1])
+      }
+    } else {
+      // 正整数添加到 seen 头部
+      seen.unshift(num)
+      k = 0 // 重置连续 -1 计数
+    }
+  }
+  return ans
+}
--- notes/2900. 最长相邻不相等子序列 I【简单】/solutions/1/1.js
+++ notes/2900. 最长相邻不相等子序列 I【简单】/solutions/1/1.js
@@ -1,1 +1,17 @@
-// todo
+/**
+ * @param {string[]} words
+ * @param {number[]} groups
+ * @return {string[]}
+ */
+var getLongestSubsequence = function (words, groups) {
+  const result = [words[0]]
+  let lastGroup = groups[0]
+  // 贪心选择：只要当前 group 与上一个不同，就选择
+  for (let i = 1; i < words.length; i++) {
+    if (groups[i] !== lastGroup) {
+      result.push(words[i])
+      lastGroup = groups[i]
+    }
+  }
+  return result
+}
