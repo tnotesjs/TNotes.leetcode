d7443fdc4eb0fd5f1864e9d6c81a8ce73703cf67
Tdahuyou <dahuyoutop@gmail.com>
2026-01-23 08:32:49 +0800

    📝 Update notes - 2026-01-23 08:32:48

--- notes/1832. 判断句子是否为全字母句【简单】/solutions/1/1.js
+++ notes/1832. 判断句子是否为全字母句【简单】/solutions/1/1.js
@@ -3,13 +3,17 @@
  * @return {boolean}
  */
 var checkIfPangram = function (sentence) {
+  // 使用位掩码记录26个字母的出现情况，每一位代表一个字母
   let mask = 0
 
   for (let i = 0; i < sentence.length; i += 1) {
     const bit = sentence.charCodeAt(i) - 97 // 'a'
     mask |= 1 << bit
+
+    // 遍历过程中如果 mask 已达到目标值，直接返回 true
     if (mask === 0x3ffffff) return true
   }
 
+  // mask 前 26 位都是 1，对应的十六进制是 0x3ffffff
   return mask === 0x3ffffff
 }
--- notes/1837. K 进制表示下的各位数字总和【简单】/solutions/1/1.js
+++ notes/1837. K 进制表示下的各位数字总和【简单】/solutions/1/1.js
@@ -7,8 +7,8 @@ var sumBase = function (n, k) {
   let ans = 0
 
   while (n > 0) {
-    ans += n % k
-    n = Math.floor(n / k)
+    ans += n % k // 取 k 进制下的最后一位数字加入总和
+    n = Math.floor(n / k) // 去掉 k 进制下的最后一位数字
   }
 
   return ans
