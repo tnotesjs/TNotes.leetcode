be11da82773fc93823af176edde30a7ada04e789
Tdahuyou <dahuyoutop@gmail.com>
2026-02-01 20:15:11 +0800

    📝 Update notes - 2026-02-01 20:15:10

--- notes/1313. 解压缩编码列表【简单】/solutions/1/1.js
+++ notes/1313. 解压缩编码列表【简单】/solutions/1/1.js
@@ -3,16 +3,12 @@
  * @return {number[]}
  */
 var decompressRLElist = function (nums) {
-  const res = []
+  const result = []
 
-  for (let i = 0; i < nums.length; i += 2) {
-    const freq = nums[i]
-    const val = nums[i + 1]
+  // 每次处理一对 [freq, val]，添加 freq 个 val 到结果数组
+  // 其中 nums[i] 是 freq，nums[i + 1] 是 val
+  for (let i = 0; i < nums.length; i += 2)
+    for (let j = 0; j < nums[i]; j++) result.push(nums[i + 1])
 
-    for (let j = 0; j < freq; j += 1) {
-      res.push(val)
-    }
-  }
-
-  return res
+  return result
 }
