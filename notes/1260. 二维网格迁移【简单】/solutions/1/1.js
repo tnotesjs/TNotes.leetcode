887e895b0854d76bc0fb3b3e49abb333693af991
Tdahuyou <dahuyoutop@gmail.com>
2026-02-06 08:41:10 +0800

    📝 Update notes - 2026-02-06 08:41:08

--- notes/1260. 二维网格迁移【简单】/solutions/1/1.js
+++ notes/1260. 二维网格迁移【简单】/solutions/1/1.js
@@ -11,8 +11,13 @@ var shiftGrid = function (grid, k) {
 
   for (let r = 0; r < m; r += 1) {
     for (let c = 0; c < n; c += 1) {
+      // 计算当前位置在一维数组中的索引
       const idx = r * n + c
+
+      // 计算迁移 k 次后在一维数组中的新索引
       const newIdx = (idx + k) % len
+
+      // 映射回二维数组的行列索引
       const nr = Math.floor(newIdx / n)
       const nc = newIdx % n
       res[nr][nc] = grid[r][c]
