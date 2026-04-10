9ba2cbd3df545210cf7490974e6dbcb4c4444e41
Tdahuyou <dahuyoutop@gmail.com>
2026-04-10 08:41:42 +0800

    📝 Update notes - 2026-04-10 08:41:41

--- notes/0079. 单词搜索【中等】/solutions/1/1.c
+++ notes/0079. 单词搜索【中等】/solutions/1/1.c
@@ -1,16 +1,22 @@
+int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
 bool dfs(char** board, int m, int n, char* word, int k, int i, int j) {
-    if (k == strlen(word)) return true;
     if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) return false;
+    if (word[k + 1] == '\0') return true;
 
     char temp = board[i][j];
     board[i][j] = '\0'; // 标记已访问
 
-    int dirs[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
-    for (int d = 0; d < 4; d++)
-        if (dfs(board, m, n, word, k + 1, i + dirs[d][0], j + dirs[d][1])) return true;
+    bool found = false;
+    for (int d = 0; d < 4; d++) {
+        if (dfs(board, m, n, word, k + 1, i + dirs[d][0], j + dirs[d][1])) {
+            found = true;
+            break;
+        }
+    }
 
     board[i][j] = temp; // 回溯恢复
-    return false;
+    return found;
 }
 
 bool exist(char** board, int boardSize, int* boardColSize, char* word) {
