69e572ebbdf0ec8dffb5933a22b037e13bb290a9
Tdahuyou <dahuyoutop@gmail.com>
2026-04-13 07:25:02 +0800

    📝 Update notes - 2026-04-12 07:25:01

--- notes/0020. 有效的括号【简单】/solutions/1/1.py
+++ notes/0020. 有效的括号【简单】/solutions/1/1.py
@@ -1,3 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        if len(s) % 2 != 0:
+            return False
+
+        stack = []
+
+        for ch in s:
+            if ch == '(':
+                stack.append(')')
+            elif ch == '[':
+                stack.append(']')
+            elif ch == '{':
+                stack.append('}')
+            else:
+                if not stack or ch != stack.pop():
+                    return False
+
+        return len(stack) == 0
