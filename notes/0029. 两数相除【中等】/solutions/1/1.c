0e8d0a7bdab464cc7c50c2a40219323ca9628b9a
Tdahuyou <dahuyoutop@gmail.com>
2026-04-16 08:43:00 +0800

    📝 Update notes - 2026-04-16 08:42:59

--- notes/0029. 两数相除【中等】/solutions/1/1.c
+++ notes/0029. 两数相除【中等】/solutions/1/1.c
@@ -1,24 +1,26 @@
 int divide(int dividend, int divisor) {
     // 唯一溢出情况：-2^31 / -1 = 2^31 超出 INT_MAX
-    if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+    if (dividend == INT_MIN && divisor == -1)
+        return INT_MAX;
 
     int sign = (dividend > 0) == (divisor > 0) ? 1 : -1;
     // 全转为负数操作，避免 -2^31 取绝对値溢出
     long long a = dividend > 0 ? -(long long)dividend : (long long)dividend;
-    long long b = divisor  > 0 ? -(long long)divisor  : (long long)divisor;
-    int ans = 0;
+    long long b = divisor > 0 ? -(long long)divisor : (long long)divisor;
+    long long ans = 0;
 
     while (a <= b) {
         long long temp = b;
-        int cnt = 1;
-        // 倍增加速：找到最大的 2^k 使得 temp*2^k >= a
+        long long cnt = 1;
+        // 倍增加速：只要当前 temp 再翻倍后仍然 >= a，就继续翻倍
         while (a - temp <= temp) {
-            temp <<= 1;
-            cnt <<= 1;
+            temp += temp; // 不能用 temp <<= 1，因为 temp 为负数，负数左移是 C
+                          // 语言标准中的未定义行为
+            cnt += cnt;
         }
         a -= temp;
         ans += cnt;
     }
 
-    return sign == 1 ? ans : -ans;
+    return sign == 1 ? (int)ans : (int)-ans;
 }
--- notes/0029. 两数相除【中等】/solutions/1/1.js
+++ notes/0029. 两数相除【中等】/solutions/1/1.js
@@ -19,10 +19,14 @@ var divide = function (dividend, divisor) {
   while (a <= b) {
     let temp = b
     let cnt = 1
-    // 倍增加速：找到最大的 2^k 使得 temp*2^k >= a
     while (a - temp <= temp) {
-      temp <<= 1
-      cnt <<= 1
+      // 不能用 <<= 做倍增，因为 JS 位运算会先把 Number 转成 32 位有符号整数
+      // 一旦结果超过 32 位范围，高位会被截断，temp 和 cnt 都可能发生符号翻转
+      // 例如 cnt = 1073741824 时，32 位二进制是 01000000 00000000 00000000 00000000
+      // 再左移一次会变成 10000000 00000000 00000000 00000000，也就是 -2147483648
+      // 这里改用加法倍增，整个计算过程都保持在 Number 的安全整数范围内
+      temp += temp
+      cnt += cnt
     }
     a -= temp
     ans += cnt
