83ae69a6df2c5c05fb4b61b6c68551345ddeef9d
Tdahuyou <dahuyoutop@gmail.com>
2026-06-15 08:21:32 +0800

    📝 Update notes - 2026-06-15 08:21:32

--- notes/0066. 加一【简单】/solutions/1/1.c
+++ notes/0066. 加一【简单】/solutions/1/1.c
@@ -1,1 +1,22 @@
-// todo
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            *returnSize = digitsSize;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+
+    // 所有位都是 9，需要新建长度为 n+1 的数组
+    int* result = (int*)malloc(sizeof(int) * (digitsSize + 1));
+    result[0] = 1;
+    for (int i = 1; i <= digitsSize; i++) {
+        result[i] = 0;
+    }
+    *returnSize = digitsSize + 1;
+    return result;
+}
--- notes/0066. 加一【简单】/solutions/1/1.py
+++ notes/0066. 加一【简单】/solutions/1/1.py
@@ -1,1 +1,10 @@
-# todo
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+
+        # 所有位都是 9，在数组开头插入 1
+        return [1] + digits
