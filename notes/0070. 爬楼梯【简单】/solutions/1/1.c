0177a98e3aae985213c0ebf01f755a7ee5951e98
Tdahuyou <dahuyoutop@gmail.com>
2026-06-17 08:35:48 +0800

    📝 Update notes - 2026-06-17 08:35:47

--- notes/0070. 爬楼梯【简单】/solutions/1/1.c
+++ notes/0070. 爬楼梯【简单】/solutions/1/1.c
@@ -1,1 +1,12 @@
-// todo
+int climbStairs(int n) {
+    if (n <= 2)
+        return n;
+
+    int prev2 = 1, prev1 = 2;
+    for (int i = 3; i <= n; i++) {
+        int current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    return prev1;
+}
--- notes/0070. 爬楼梯【简单】/solutions/1/1.py
+++ notes/0070. 爬楼梯【简单】/solutions/1/1.py
@@ -1,1 +1,9 @@
-# todo
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        prev2, prev1 = 1, 2
+        for _ in range(3, n + 1):
+            prev2, prev1 = prev1, prev1 + prev2
+        return prev1
