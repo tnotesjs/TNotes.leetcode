85ebe9ed83c80c933330976b0fe7402dd13623b1
Tdahuyou <dahuyoutop@gmail.com>
2026-06-22 19:52:28 +0800

    📝 Update notes - 2026-06-22 19:52:27

--- notes/0097. 交错字符串【中等】/solutions/1/1.c
+++ notes/0097. 交错字符串【中等】/solutions/1/1.c
@@ -1,16 +1,22 @@
 bool isInterleave(char* s1, char* s2, char* s3) {
     int m = strlen(s1), n = strlen(s2);
-    if (m + n != strlen(s3)) return false;
-    bool dp[n + 1];
+    if (m + n != strlen(s3))
+        return false;
+
+    bool dp[m + 1][n + 1];
     memset(dp, false, sizeof(dp));
-    dp[0] = true;
+    dp[0][0] = true;
+
     for (int j = 1; j <= n; j++)
-        dp[j] = dp[j - 1] && s2[j - 1] == s3[j - 1];
+        dp[0][j] = dp[0][j - 1] && s2[j - 1] == s3[j - 1];
+
     for (int i = 1; i <= m; i++) {
-        dp[0] = dp[0] && s1[i - 1] == s3[i - 1];
-        for (int j = 1; j <= n; j++)
-            dp[j] = (dp[j] && s1[i - 1] == s3[i + j - 1]) ||
-                    (dp[j - 1] && s2[j - 1] == s3[i + j - 1]);
+        dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
+        for (int j = 1; j <= n; j++) {
+            dp[i][j] = (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
+                       (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+        }
     }
-    return dp[n];
+
+    return dp[m][n];
 }
--- notes/0097. 交错字符串【中等】/solutions/1/1.js
+++ notes/0097. 交错字符串【中等】/solutions/1/1.js
@@ -5,18 +5,24 @@
  * @return {boolean}
  */
 var isInterleave = function (s1, s2, s3) {
-  const m = s1.length,
-    n = s2.length
+  const m = s1.length
+  const n = s2.length
   if (m + n !== s3.length) return false
-  const dp = new Array(n + 1).fill(false)
-  dp[0] = true
-  for (let j = 1; j <= n; j++) dp[j] = dp[j - 1] && s2[j - 1] === s3[j - 1]
+
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(false))
+  dp[0][0] = true
+
+  for (let j = 1; j <= n; j++)
+    dp[0][j] = dp[0][j - 1] && s2[j - 1] === s3[j - 1]
+
   for (let i = 1; i <= m; i++) {
-    dp[0] = dp[0] && s1[i - 1] === s3[i - 1]
-    for (let j = 1; j <= n; j++)
-      dp[j] =
-        (dp[j] && s1[i - 1] === s3[i + j - 1]) ||
-        (dp[j - 1] && s2[j - 1] === s3[i + j - 1])
+    dp[i][0] = dp[i - 1][0] && s1[i - 1] === s3[i - 1]
+    for (let j = 1; j <= n; j++) {
+      dp[i][j] =
+        (dp[i - 1][j] && s1[i - 1] === s3[i + j - 1]) ||
+        (dp[i][j - 1] && s2[j - 1] === s3[i + j - 1])
+    }
   }
-  return dp[n]
+
+  return dp[m][n]
 }
