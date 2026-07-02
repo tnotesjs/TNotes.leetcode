df8195db82118f56e53327fb2bb98bda0a4d416a
Tdahuyou <dahuyoutop@gmail.com>
2026-07-02 21:39:18 +0800

    📝 Update notes - 2026-07-02 21:39:17

--- notes/0111. 二叉树的最小深度【简单】/solutions/1/1.c
+++ notes/0111. 二叉树的最小深度【简单】/solutions/1/1.c
@@ -1,1 +1,24 @@
-// todo
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int minDepth(struct TreeNode* root) {
+    if (!root)
+        return 0;
+
+    if (!root->left && !root->right)
+        return 1;
+
+    if (!root->left)
+        return minDepth(root->right) + 1;
+    if (!root->right)
+        return minDepth(root->left) + 1;
+
+    int leftDepth = minDepth(root->left);
+    int rightDepth = minDepth(root->right);
+    return (leftDepth < rightDepth ? leftDepth : rightDepth) + 1;
+}
