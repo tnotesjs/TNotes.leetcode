9aa03861dfd8699ed8ab2a78ee0235be006c0cf0
Tdahuyou <dahuyoutop@gmail.com>
2026-07-06 07:09:24 +0800

    📝 Update notes - 2026-07-05 07:09:24

--- notes/0121. 买卖股票的最佳时机【简单】/solutions/1/1.c
+++ notes/0121. 买卖股票的最佳时机【简单】/solutions/1/1.c
@@ -1,1 +1,14 @@
-// todo
+int maxProfit(int* prices, int pricesSize) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < pricesSize; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+
+    return maxProfit;
+}
