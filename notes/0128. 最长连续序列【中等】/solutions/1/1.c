54dda81f715043e0a78b3f8beebbb2df80cf4111
Tdahuyou <dahuyoutop@gmail.com>
2026-07-06 07:59:18 +0800

    📝 Update notes - 2026-07-05 07:59:17

--- notes/0128. 最长连续序列【中等】/solutions/1/1.c
+++ notes/0128. 最长连续序列【中等】/solutions/1/1.c
@@ -1,6 +1,9 @@
 int longestConsecutive(int* nums, int numsSize) {
-    if (numsSize == 0) return 0;
+    if (numsSize == 0)
+        return 0;
     // 排序后遍历（C 没有内置哈希集合，用排序代替）
+    // 时间复杂度：O(nlogn)
+    // 空间复杂度：O(1)
     int cmp(const void* a, const void* b) {
         int x = *(int*)a, y = *(int*)b;
         return (x > y) - (x < y);
@@ -8,13 +11,15 @@ int longestConsecutive(int* nums, int numsSize) {
     qsort(nums, numsSize, sizeof(int), cmp);
     int maxLen = 1, len = 1;
     for (int i = 1; i < numsSize; i++) {
-        if (nums[i] == nums[i - 1]) continue; // 跳过重复
+        if (nums[i] == nums[i - 1])
+            continue; // 跳过重复
         if (nums[i] == nums[i - 1] + 1) {
             len++;
         } else {
             len = 1;
         }
-        if (len > maxLen) maxLen = len;
+        if (len > maxLen)
+            maxLen = len;
     }
     return maxLen;
 }
