3bceb3e562f0935508f0548371d429dcece13e68
Tdahuyou <dahuyoutop@gmail.com>
2026-07-05 20:05:16 +0800

    📝 Update notes - 2026-07-05 20:05:15

--- notes/0118. 杨辉三角【简单】/solutions/1/1.c
+++ notes/0118. 杨辉三角【简单】/solutions/1/1.c
@@ -1,1 +1,26 @@
-// todo
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume
+ * caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    int** triangle = (int**)malloc(sizeof(int*) * numRows);
+    *returnColumnSizes = (int*)malloc(sizeof(int) * numRows);
+    *returnSize = numRows;
+
+    for (int i = 0; i < numRows; i++) {
+        triangle[i] = (int*)malloc(sizeof(int) * (i + 1));
+        (*returnColumnSizes)[i] = i + 1;
+        triangle[i][0] = 1;
+        triangle[i][i] = 1;
+    }
+
+    for (int r = 2; r < numRows; r++) {
+        for (int c = 1; c < r; c++) {
+            triangle[r][c] = triangle[r - 1][c - 1] + triangle[r - 1][c];
+        }
+    }
+
+    return triangle;
+}
--- notes/0118. 杨辉三角【简单】/solutions/1/1.py
+++ notes/0118. 杨辉三角【简单】/solutions/1/1.py
@@ -1,1 +1,9 @@
-# todo
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        triangle = [[1] * (i + 1) for i in range(numRows)]
+
+        for r in range(2, numRows):
+            for c in range(1, r):
+                triangle[r][c] = triangle[r - 1][c - 1] + triangle[r - 1][c]
+
+        return triangle
