0592780cf93459f7aecd8326648af4825e5d98b7
Tdahuyou <dahuyoutop@gmail.com>
2026-07-09 07:18:20 +0800

    📝 Update notes - 2026-07-08 07:18:19

--- notes/0136. 只出现一次的数字【简单】/solutions/1/1.c
+++ notes/0136. 只出现一次的数字【简单】/solutions/1/1.c
@@ -1,1 +1,9 @@
-// todo
+int singleNumber(int* nums, int numsSize) {
+    int result = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i];
+    }
+
+    return result;
+}
--- notes/0136. 只出现一次的数字【简单】/solutions/1/1.py
+++ notes/0136. 只出现一次的数字【简单】/solutions/1/1.py
@@ -1,1 +1,8 @@
-# todo
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        result = 0
+
+        for num in nums:
+            result ^= num
+
+        return result
