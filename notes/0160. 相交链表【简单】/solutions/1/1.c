b7df6bc144e1cbadeafec567eb0d7caa8c4386dc
Tdahuyou <dahuyoutop@gmail.com>
2026-07-27 08:11:44 +0800

    📝 Update notes - 2026-07-27 08:11:43

--- notes/0160. 相交链表【简单】/solutions/1/1.c
+++ notes/0160. 相交链表【简单】/solutions/1/1.c
@@ -1,1 +1,20 @@
-// todo
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* headB) {
+    if (!headA || !headB) return NULL;
+
+    struct ListNode* pA = headA;
+    struct ListNode* pB = headB;
+
+    while (pA != pB) {
+        pA = pA ? pA->next : headB;
+        pB = pB ? pB->next : headA;
+    }
+
+    return pA;
+}
