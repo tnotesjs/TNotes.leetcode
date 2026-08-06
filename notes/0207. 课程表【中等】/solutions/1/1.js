e1567a88b4795297111540145aece3a9dc113e57
Tdahuyou <dahuyoutop@gmail.com>
2026-08-06 16:22:35 +0800

    📝 Update notes - 2026-08-06 16:22:34

--- notes/0207. 课程表【中等】/solutions/1/1.js
+++ notes/0207. 课程表【中等】/solutions/1/1.js
@@ -4,16 +4,19 @@
  * @return {boolean}
  */
 var canFinish = function (numCourses, prerequisites) {
+  // 建图 + 入度
   const inDegree = new Array(numCourses).fill(0)
   const graph = Array.from({ length: numCourses }, () => [])
   for (const [a, b] of prerequisites) {
-    graph[b].push(a)
-    inDegree[a]++
+    graph[b].push(a) // 修完 b 之后可以解锁哪些课
+    inDegree[a]++ // 解锁课 a 还差几门先修课
   }
+  // 把「已经能修」的课入队
   const queue = []
   for (let i = 0; i < numCourses; i++) {
     if (inDegree[i] === 0) queue.push(i)
   }
+  // BFS 不断「修课」
   let count = 0
   while (queue.length) {
     const cur = queue.shift()
